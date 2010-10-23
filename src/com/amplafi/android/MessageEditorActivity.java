04beb4bf5cd6d79a56dba5d69e6cee07d6991654
patmoore <patmoore@ddf3920b-8537-492c-b414-20e08c352593>
2010-10-23 21:26:15 +0000

    more android
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-android/trunk@4 ddf3920b-8537-492c-b414-20e08c352593

--- src/com/amplafi/android/MessageEditorActivity.java
+++ src/com/amplafi/android/MessageEditorActivity.java
@@ -1,13 +1,45 @@
 package com.amplafi.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MessageEditorActivity extends Activity {
-    /** Called when the activity is first created. */
+    private Button mSubmitButton;
+    private Button mEditButton;
+    private Button mGetButton;
+
+    private Context mContext;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_edit_screen);
+        mContext = this;
+
+        mSubmitButton = (Button) findViewById(R.id.send_text_to_server);
+        mEditButton   = (Button) findViewById(R.id.send_edit_to_server);
+        mGetButton    = (Button) findViewById(R.id.get_texts_from_server);
+
+        mSubmitButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                sendTextToServer();
+            }
+
+        });
+    }
+
+    private void sendTextToServer() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+        builder.setTitle("Send To Server");
+        builder.setMessage("Send my message to the Server")
+            .create().show();
     }
 }
