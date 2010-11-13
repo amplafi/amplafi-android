5478018ed47f7435c490ed3015af391bc2c124c1
louis <louis@ddf3920b-8537-492c-b414-20e08c352593>
2010-11-13 23:46:21 +0000

    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-android/trunk@6 ddf3920b-8537-492c-b414-20e08c352593

--- src/com/amplafi/android/task/SendTextTask.java
+++ src/com/amplafi/android/task/SendTextTask.java
@@ -4,13 +4,32 @@
  */
 package com.amplafi.android.task;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpConnection;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.DefaultHttpClientConnection;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.AsyncTask;
+import android.util.Log;
 
 /**
  * @author patmoore
  *
  */
 public class SendTextTask extends AsyncTask<String, Void, String> {
+    private static final String TAG = "SendTextTask";
+    
     public interface SendTextRequestor {
         public void taskStarted();
         public void taskCancelled();
@@ -30,18 +49,52 @@ public class SendTextTask extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... testToSend) {
-        // create an Http call to the service
-        // POST
-        // get the answer
-        // return the JSON from the answer as the return value of this method
-        return "I'm done";
+    protected String doInBackground(String... params) {
+        String output = "I'm done.";
+        InputStream iStream = getData(params);
+        try {
+            output = IOUtils.toString(iStream);
+        } catch (IOException e) {
+            output = e.getMessage();
+        }
+        
+        return output;
+    }
+    
+    private InputStream getData(String[] params) {
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(URI.create(makeAmplafiUrl(params)));
+        HttpResponse response;
+        InputStream iStream = null;
+        try {
+            response = client.execute(request);
+            iStream = response.getEntity().getContent();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return iStream;
+    }
+    
+    static private String makeAmplafiUrl(String[] params) {
+        URL amplafiUrl = null;
+        try {
+            amplafiUrl = new URL("http://amplafi.net/apiv1/" 
+                + params[0] + "?" + params[1]);
+        } catch (MalformedURLException malEx) {
+            malEx.printStackTrace();
+        }
+        
+        return amplafiUrl.toString();
     }
 
     @Override
     protected void onPostExecute(String result) {
         mRequestor.taskCompleted(result);
     }
-
-
 }
+
