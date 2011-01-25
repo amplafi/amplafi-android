1ae6e6b344508d248517e9e2c13a465b25001d76
aectann <aectann@aelinux.site>
2011-01-25 20:43:26 +0800

    Using Android SDK provided classes to get response contents.

--- src/com/amplafi/android/task/SendTextTask.java
+++ src/com/amplafi/android/task/SendTextTask.java
@@ -4,25 +4,21 @@
  */
 package com.amplafi.android.task;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpConnection;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.DefaultHttpClientConnection;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 /**
  * @author patmoore
@@ -52,35 +48,22 @@ public class SendTextTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
         String output = "I'm done.";
-        InputStream iStream = getData(params);
         try {
-            output = IOUtils.toString(iStream);
-            JSONObject jsonObject = new JSONObject(output);
-            
+            JSONObject jsonObject = new JSONObject(getData(params));
         } catch (Exception e) {
-            output = e.getMessage();
+            throw new IllegalStateException("Error while trying to access " + params);
         }
         
         return output;
     }
     
-    private InputStream getData(String[] params) {
+    private String getData(String[] params) throws IOException {
         HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet(URI.create(makeAmplafiUrl(params)));
-        HttpResponse response;
-        InputStream iStream = null;
-        try {
-            response = client.execute(request);
-            iStream = response.getEntity().getContent();
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        return iStream;
+        HttpResponse response = client.execute(request);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        response.getEntity().writeTo(out);
+        return out.toString();
     }
     
     static private String makeAmplafiUrl(String[] params) {
